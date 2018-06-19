d32fd1d4e5a6efecd065cf189c5693ca3cc609be
Pc <Pc@DESKTOP-9J6992O>
2018-06-19 10:54:24 +0200

    aggiunta acquista Prodotto

--- src/main/java/it/dstech/Supermarket/service/auth/ProdottoService.java
+++ src/main/java/it/dstech/Supermarket/service/auth/ProdottoService.java
@@ -4,10 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import it.dstech.Supermarket.model.Categoria;
 import it.dstech.Supermarket.model.Prodotto;
+import it.dstech.Supermarket.model.User;
 import it.dstech.Supermarket.repository.IProdottoRepository;
 
 @Service
@@ -18,6 +21,9 @@ public class ProdottoService {
 	
 	@Autowired
 	private StoricoService service;
+	
+	@Autowired
+	private UserService serviceUser;
 	
 	public Iterable<Prodotto> salvaProdotto (ArrayList<Prodotto> listaProdotti){
 		return dao.save(listaProdotti);
@@ -45,5 +51,31 @@ public class ProdottoService {
 	public Iterable<Prodotto> prodottiPerCategoria (Categoria categoria){
 		List<Prodotto> listaProdotti = dao.findByCategoria(categoria);
 		return listaProdotti;
+	}
+	public void acquistoProdotti(String numeroCarta, String cvv, ArrayList<Integer> idProdotti ) throws Exception{
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		User user = serviceUser.findByUsername(auth.getName());
+		for (Integer id : idProdotti) {
+			Prodotto prodotto = dao.findOne(id);
+			if(id != null) {
+				for(int i = 0; i < user.getListaCarteCredito().size(); i++) {
+					if(user.getListaCarteCredito().get(i).getNumero().equals(numeroCarta) && user.getListaCarteCredito().get(i).getCvv().equals(cvv)) {
+						if(user.getListaCarteCredito().get(i).getCredito()>= prodotto.getPrezzoUnitario()) {
+							prodotto.setQuantitaDisponibile(prodotto.getQuantitaDisponibile()-1);
+							user.getListaCarteCredito().get(i).setCredito(user.getListaCarteCredito().get(i).getCredito() - (prodotto.getPrezzoUnitario()));
+							//ragazzi, manca aggiungere questa transazione allo storico...
+							
+							serviceUser.save(user);
+						}else {
+							throw new Exception ("Insuficient credit");
+						}
+					}else {
+						throw new Exception ("Card not found");
+					}
+				}
+			}else {
+				throw new Exception ("Product not found");
+			}
+		}
 	}
 }
